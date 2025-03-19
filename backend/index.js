d21021fcb1f7e1ff3e39ba6104fe3c5182e7aa51
shreedhar001 <kano78bei007@kec.edu.np>
2025-03-19 08:40:04 +0000

    feat: update CORS configuration to allow requests from 127.0.0.1

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
